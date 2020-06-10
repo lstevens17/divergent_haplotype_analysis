acefb7cf40f16ecf495246c8871ac22eb70153df
s1035490 <lewis.stevens07@gmail.com>
2020-06-10 20:06:43 +0100

    update plot dimensions

--- scripts/plotheatmap.R
+++ scripts/plotheatmap.R
@@ -15,12 +15,12 @@ aa_dist_m <- as.matrix(aa_dist)
 colnames(aa_dist_m) <- rownames(aa_dist_m)
 
 # create plot file
-png(file=paste(args[1], ".png", sep = ""))
+png(file=paste(args[1], ".png", sep = ""), width = 1000, height = 1000, units = "px")
 
 # set color palette 
 hmcols<-colorRampPalette(c("red","blue"))(51)
 
 # plot heatmap 
-heatmap.2(aa_dist_m, col=hmcols, density.info="none", trace="none", breaks=c(seq(94.9,100,0.1)), key=FALSE)
+heatmap.2(aa_dist_m, col=hmcols, density.info="none", trace="none", breaks=c(seq(94.9,100,0.1)), key=FALSE, margins = c(13, 13))
 
 dev.off()
